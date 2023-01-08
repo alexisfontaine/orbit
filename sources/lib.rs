4fa179420e533f66cacae05848a5cabba90ab976
Alexis Fontaine <alexis.fontaine@outlook.com>
2023-01-08 16:23:01 +0100

    Prevent simultaneous features `canvas` and `svg`

--- sources/lib.rs
+++ sources/lib.rs
@@ -5,6 +5,10 @@
 )]
 
 
+#[cfg(all(feature = "canvas", feature = "svg"))]
+compile_error!("feature \"canvas\" and feature \"svg\" cannot be enabled at the same time");
+
+
 pub mod components;
 pub mod model;
 pub mod state;
